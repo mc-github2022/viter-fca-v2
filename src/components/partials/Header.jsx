57e99a826affbaed793260c834223e66613972af
mc-github2022 <mark.bumagat@frontlinebusiness.com.ph>
2023-12-20 13:28:41 +0800

    mclocal

--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Logo from "../../../public/fcalogo.png";
 import { FaUserCircle } from "react-icons/fa";
+import LogoGreen from "./svg/Logo";
 
 const Header = () => {
   return (
     <>
       <header className="py-2 px-4 relative">
         <div className="flex justify-between items-center">
-          <img className="h-[70px] w-auto" src={Logo} alt="" />
+          <LogoGreen />
           <div className="header__avatar flex items-center gap-2 md:border-2 md:border-gray md:py-1 md:px-2 rounded-lg cursor-pointer">
             <div className="userName hidden md:block leading-[1] text-right">
               <p className="mb-0 text-black font-semibold">MC</p>
