ea2f1ec08c7e3136825e0a2d8959f84355ec451d
monmonplaza <monmon.plaza@gmail.com>
2023-12-28 08:03:45 +0800

    123

--- src/components/pages/developer/settings/Department/Department.jsx
+++ src/components/pages/developer/settings/Department/Department.jsx
@@ -1,69 +1,46 @@
-import Footer from "@/components/partials/Footer.jsx";
-import Header from "@/components/partials/Header.jsx";
-import Navigation from "@/components/partials/Navigation.jsx";
-import Toast from "@/components/partials/Toast.jsx";
-import ModalValidate from "@/components/partials/modals/ModalValidate.jsx";
-import { StoreContext } from "@/components/store/StoreContext.jsx";
+import BreadCrumbs from "@/components/partials/BreadCrumbs";
+import Footer from "@/components/partials/Footer";
+import Header from "@/components/partials/Header";
+import Navigation from "@/components/partials/Navigation";
 import React from "react";
 import {
-  setIsAdd,
-  setIsSettingsOpen,
-} from "@/components/store/StoreAction.jsx";
-import { FaSearch } from "react-icons/fa";
-import SearchBar from "@/components/partials/SearchBar";
-import RecordCount from "@/components/partials/RecordCount";
-import UsersTable from "./DepartmentTable";
-import ModalAddDepartment from "./ModalAddDepartment";
-import useQueryData from "@/components/custom-hooks/useQueryData";
-import DepartmentTable from "./DepartmentTable";
+  FaAngleLeft,
+  FaCaretLeft,
+  FaPlus,
+  FaRegCaretSquareLeft,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
+import DepartmentTable from "./DepartmentTable.jsx";
 
-const Department = () => {
-  const { store, dispatch } = React.useContext(StoreContext);
-  const [itemEdit, setItemEdit] = React.useState(null);
-
-  // const { data: roles } = useQueryData(
-  //   "/v1/user-system/roles", // endpoint
-  //   "get", // method
-  //   "system-role" // key
-  // );
-
-  React.useEffect(() => {
-    dispatch(setIsSettingsOpen(true));
-  }, []);
-
-  const handleAdd = () => {
-    dispatch(setIsAdd(true));
-    setItemEdit(null);
-  };
+const Students = () => {
   return (
     <>
       <Header />
-      <main className="bg-secondary main__wrapper h-[calc(100vh-48px)]">
-        <div className="grid grid-cols-[180px_1fr] gap-10 h-full ">
-          <Navigation menu="settings" submenu="settingsDepartment" />
-          <div className="pr-10 pt-5 relative">
-            <div className="breadcrumbs"></div>
-            <div className="flex justify-between items-center mb-5">
-              <div>
-                <h1 className="leading-none mb-2">Department</h1>
-                <p className="mb-0">Set Department</p>
-              </div>
-              <button className="btn btn--accent btn--sm" onClick={handleAdd}>
-                Add
-              </button>
-            </div>
-            <SearchBar />
-            <DepartmentTable setItemEdit={setItemEdit} />
-            <Footer />
-          </div>
-        </div>
-      </main>
+      <section className="main__wrap flex relative ">
+        <Navigation />
 
-      {store.isAdd && <ModalAddDepartment itemEdit={itemEdit} />}
-      {store.validate && <ModalValidate />}
-      {store.success && <Toast />}
+        <main className="main__content mt-[65px]">
+          <div className="main__header flex justify-between items-start lg:items-center">
+            <div>
+              <Link to="/" className="flex gap-1 items-center lg:hidden">
+                <FaAngleLeft /> Back
+              </Link>
+              <BreadCrumbs />
+              <h1 className="text-clampH1 mb-0">Department</h1>
+              <p className="mb-4 text-xs hidden lg:block">Set New Department</p>
+            </div>
+            <button className="btn btn--accent btn--sm mt-1">
+              Add <FaPlus />
+            </button>
+          </div>
+
+          <DepartmentTable />
+        </main>
+
+        <Footer />
+      </section>
     </>
   );
 };
 
-export default Department;
+export default Students;
--- src/components/pages/developer/settings/Department/DepartmentTable.jsx
+++ src/components/pages/developer/settings/Department/DepartmentTable.jsx
@@ -1,223 +1,70 @@
-import useQueryData from "@/components/custom-hooks/useQueryData";
-import RecordCount from "@/components/partials/RecordCount";
-import TableSpinner from "@/components/partials/spinners/TableSpinner";
-import { StoreContext } from "@/components/store/StoreContext";
-import React, { Fragment } from "react";
-import {
-  setIsAdd,
-  setIsConfirm,
-  setIsRestore,
-  setQuickEditID,
-} from "../../../../store/StoreAction.jsx";
+import Pills from "@/components/partials/Pills.jsx";
+import SearchBar from "@/components/partials/SearchBar";
+import React from "react";
+import { BsThreeDotsVertical } from "react-icons/bs";
 import { FiArchive, FiEdit3 } from "react-icons/fi";
-import { MdOutlinePersonOff, MdRestore } from "react-icons/md";
-import { PiKey } from "react-icons/pi";
+import { MdRestore } from "react-icons/md";
 import { RiDeleteBinLine } from "react-icons/ri";
-import { getDepartmentCountRecord } from "./functions-department";
-import TableLoading from "@/components/partials/TableLoading";
-import NoData from "@/components/partials/NoData";
-import ServerError from "@/components/partials/ServerError";
-import Pills from "@/components/partials/Pills";
-import ModalDeleteAndRestore from "@/components/partials/modals/ModalDeleteAndRestore.jsx";
-import ModalConfirm from "@/components/partials/modals/ModalConfirm.jsx";
-import QuickEditDepartment from "./QuickEditDepartment.jsx";
 
 const DepartmentTable = () => {
-  const { store, dispatch } = React.useContext(StoreContext);
-  const [dataItem, setData] = React.useState(null);
-  const [id, setId] = React.useState(null);
-  const [isDel, setDel] = React.useState(false);
-  let counter = 1;
-
-  const {
-    isLoading,
-    isFetching,
-    error,
-    data: department,
-  } = useQueryData(
-    "/v2/dev-department", // endpoint
-    "get", // method
-    "settings-department" // key
-  );
-
-  const handleArchive = (item) => {
-    dispatch(setIsConfirm(true));
-    setId(item.department_aid);
-    setData(item);
-    setDel(null);
-  };
-
-  const handleRestore = (item) => {
-    dispatch(setIsRestore(true));
-    setId(item.department_aid);
-    setData(item);
-    setDel(null);
-  };
-
-  const handleDelete = (item) => {
-    dispatch(setIsRestore(true));
-    setId(item.department_aid);
-    setData(item);
-    setDel(true);
-  };
-
-  const handleQuickEdit = (item) => {
-    dispatch(setQuickEditID(item.department_aid));
-    setId(item.department_aid);
-    setData(item);
-  };
-
   return (
     <>
-      <div className="table__wrapper relative rounded-md shadow-md overflow-x-auto">
-        {isFetching && !isLoading && <TableSpinner />}
+      <div className="main__table">
+        <div className="table__wrapper mb-[80px]">
+          <table>
+            <thead>
+              <tr>
+                <th className="w-[30px]">#</th>
+                <th>Name</th>
+                <th>Status</th>
+                <th className="table__action">
+                  <span className="lg:hidden">Action</span>
+                </th>
+              </tr>
+            </thead>
 
-        <RecordCount
-          record={department?.count}
-          status={getDepartmentCountRecord(department)}
-        />
-        <table>
-          <thead>
-            <tr className="bg-[#e5e7eb]">
-              <th>#</th>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Satus</th>
-              <th className="action"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {(isLoading || department?.data.length === 0) && (
-              <tr className="text-center ">
-                <td colSpan="100%" className="p-2 md:p-10">
-                  {isLoading ? (
-                    <TableLoading count={20} cols={3} />
-                  ) : (
-                    <NoData />
-                  )}
+            <tbody>
+              <tr>
+                <td className="w-[30px]">1</td>
+                <td className="whitespace-nowrap">Bernanrdino, Mark John</td>
+                <td>
+                  <Pills color="text-success" label="Active" />
+                </td>
+                <td className="table__action">
+                  <span className="border border-gray-100 text-base p-1 hidden  lg:grid w-[30px] place-content-center rounded-md">
+                    <BsThreeDotsVertical />
+                  </span>
+                  <ul className="">
+                    <li>
+                      <button className="tooltip" data-tooltip="Edit">
+                        <FiEdit3 />
+                      </button>
+                    </li>
+
+                    <li>
+                      <button className="tooltip" data-tooltip="Archive">
+                        <FiArchive />
+                      </button>
+                    </li>
+
+                    <li>
+                      <button className="tooltip" data-tooltip="Delete">
+                        <RiDeleteBinLine />
+                      </button>
+                    </li>
+
+                    <li>
+                      <button className="tooltip" data-tooltip="Restore">
+                        <MdRestore />
+                      </button>
+                    </li>
+                  </ul>
                 </td>
               </tr>
-            )}
-
-            {error && (
-              <tr className="text-center ">
-                <td colSpan="100%" className="p-10">
-                  <ServerError />
-                </td>
-              </tr>
-            )}
-            {department?.data.map((item, key) => {
-              return (
-                <Fragment key={key}>
-                  <tr>
-                    <td>{counter++}.</td>
-                    <td>{item.department_name}</td>
-                    <td>{item.department_description}</td>
-                    <td>
-                      {item.department_active === 1 ? (
-                        <Pills label="Active" textColor="text-success" />
-                      ) : (
-                        <Pills label="Inactive" textColor="text-archive" />
-                      )}
-                    </td>
-
-                    <td
-                      className="table__action top-4 right-5 "
-                      data-ellipsis=". . ."
-                    >
-                      <ul className="flex items-center justify-end gap-2 mr-2">
-                        {item.department_active === 1 ? (
-                          <>
-                            <li>
-                              <button
-                                className="tooltip"
-                                data-tooltip="Edit"
-                                onClick={() => handleQuickEdit(item)}
-                              >
-                                <FiEdit3 />
-                              </button>
-                            </li>
-                            <li>
-                              <button
-                                className="tooltip"
-                                data-tooltip="Archive"
-                                onClick={() => handleArchive(item)}
-                              >
-                                <FiArchive />
-                              </button>
-                            </li>
-                          </>
-                        ) : (
-                          <>
-                            <li>
-                              <button
-                                className="tooltip"
-                                data-tooltip="Delete"
-                                onClick={() => handleDelete(item)}
-                              >
-                                <RiDeleteBinLine />
-                              </button>
-                            </li>
-                            <li>
-                              <button
-                                className="tooltip"
-                                data-tooltip="Restore"
-                                onClick={() => handleRestore(item)}
-                              >
-                                <MdRestore />
-                              </button>
-                            </li>
-                          </>
-                        )}
-                      </ul>
-                    </td>
-                  </tr>
-
-                  <tr>
-                    <td colSpan={4} className="p-0 w-full">
-                      {dataItem !== null &&
-                        item.department_aid === store.quickEditID && (
-                          <QuickEditDepartment
-                            dataItem={dataItem}
-                            setData={setData}
-                            endpoint={`/v2/dev-department/${dataItem.department_aid}`}
-                            queryKey="settings-department"
-                            id={id}
-                          />
-                        )}
-                    </td>
-                  </tr>
-                </Fragment>
-              );
-            })}
-          </tbody>
-        </table>
+            </tbody>
+          </table>
+        </div>
       </div>
-
-      {store.isConfirm && (
-        <ModalConfirm
-          mysqlApiArchive={`/v2/dev-department/active/${id}`}
-          msg={"Are you sure you want to archive this department?"}
-          item={dataItem.department_name}
-          queryKey={"settings-department"}
-        />
-      )}
-
-      {store.isRestore && (
-        <ModalDeleteAndRestore
-          id={id}
-          isDel={isDel}
-          mysqlApiDelete={`/v2/dev-department/${id}`}
-          mysqlApiRestore={`/v2/dev-department/active/${id}`}
-          msg={
-            isDel
-              ? "Are you sure you want to delete this department?"
-              : "Are you sure you want to restore this department?"
-          }
-          item={dataItem.department_name}
-          queryKey={"settings-department"}
-        />
-      )}
     </>
   );
 };
