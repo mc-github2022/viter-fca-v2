5b17238eeb28894b845a95f95d1e02fd81dd99ae
mc-github2022 <mark.bumagat@frontlinebusiness.com.ph>
2024-02-02 12:45:57 +0800

    localmc

--- src/components/pages/developer/settings/requirement-finance/RequirementFinanceList.jsx
+++ src/components/pages/developer/settings/requirement-finance/RequirementFinanceList.jsx
@@ -18,6 +18,7 @@ import ModalDelete from "@/components/partials/modals/ModalDelete.jsx";
 import React from "react";
 import { FiEdit2, FiTrash } from "react-icons/fi";
 import { MdOutlineRestore } from "react-icons/md";
+import ModalInvalidRequestError from "@/components/partials/modals/ModalInvalidRequestError";
 const RequirementFinanceList = ({ setItemEdit }) => {
   const { store, dispatch } = React.useContext(StoreContext);
   const [dataItem, setData] = React.useState(null);
@@ -69,70 +70,78 @@ const RequirementFinanceList = ({ setItemEdit }) => {
       <div className="datalist max-w-[650px] w-full overflow-x-hidden overflow-y-auto max-h-[450px] lg:max-h-[580px] custom__scroll  poco:max-h-[640px] lg:poco:max-h-[400px]">
         {isFetching && !isLoading && <TableSpinner />}
 
-        {(isLoading || finance?.data.length === 0) &&
-          (isLoading ? <TableLoading count={20} cols={3} /> : <NoData />)}
-        {finance?.data.map((item, key) => (
-          <div
-            className={
-              "datalist__item text-xs  flex justify-between lg:items-center border-b border-line py-2 first:pt-5 lg:flex-row last:border-none"
-            }
-            key={key}
-          >
+        {!isLoading && finance.success === false ? (
+          <ModalInvalidRequestError />
+        ) : isLoading ? (
+          <TableLoading count={20} cols={3} />
+        ) : finance?.data.length === 0 ? (
+          <NoData />
+        ) : (
+          !isLoading &&
+          finance.success === true &&
+          finance?.data.map((item, key) => (
             <div
-              className={`${
-                item.requirement_finance_active ? "opacity-100" : "opacity-40"
-              } `}
+              className={
+                "datalist__item text-xs  flex justify-between lg:items-center border-b border-line py-2 first:pt-5 lg:flex-row last:border-none"
+              }
+              key={key}
             >
-              <p className="mb-1">{item.requirement_finance_name}</p>
-            </div>
+              <div
+                className={`${
+                  item.requirement_finance_active ? "opacity-100" : "opacity-40"
+                } `}
+              >
+                <p className="mb-1">{item.requirement_finance_name}</p>
+              </div>
 
-            <ul className="datalist__action flex items-center gap-1 pr-3 ">
-              {item.requirement_finance_active === 1 ? (
-                <>
-                  <li className=" ">
-                    <button
-                      className="tooltip"
-                      data-tooltip="Edit"
-                      onClick={() => handleEdit(item)}
-                    >
-                      <FiEdit2 />
-                    </button>
-                  </li>
-                  <li>
-                    <button
-                      className="tooltip"
-                      data-tooltip="Archive"
-                      onClick={() => handleArchive(item)}
-                    >
-                      <BsArchive />
-                    </button>
-                  </li>
-                </>
-              ) : (
-                <>
-                  <li className=" ">
-                    <button
-                      className="tooltip"
-                      data-tooltip="Restore"
-                      onClick={() => handleRestore(item)}
-                    >
-                      <MdOutlineRestore className="text-base" />
-                    </button>
-                  </li>
-                  <li>
-                    <button
-                      className="tooltip"
-                      data-tooltip="Delete"
-                      onClick={() => handleDelete(item)}
-                    >
-                      <FiTrash />
-                    </button>
-                  </li>
-                </>
-              )}
-            </ul>
-          </div>
-        ))}
+              <ul className="datalist__action flex items-center gap-1 pr-3 ">
+                {item.requirement_finance_active === 1 ? (
+                  <>
+                    <li className=" ">
+                      <button
+                        className="tooltip"
+                        data-tooltip="Edit"
+                        onClick={() => handleEdit(item)}
+                      >
+                        <FiEdit2 />
+                      </button>
+                    </li>
+                    <li>
+                      <button
+                        className="tooltip"
+                        data-tooltip="Archive"
+                        onClick={() => handleArchive(item)}
+                      >
+                        <BsArchive />
+                      </button>
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li className=" ">
+                      <button
+                        className="tooltip"
+                        data-tooltip="Restore"
+                        onClick={() => handleRestore(item)}
+                      >
+                        <MdOutlineRestore className="text-base" />
+                      </button>
+                    </li>
+                    <li>
+                      <button
+                        className="tooltip"
+                        data-tooltip="Delete"
+                        onClick={() => handleDelete(item)}
+                      >
+                        <FiTrash />
+                      </button>
+                    </li>
+                  </>
+                )}
+              </ul>
+            </div>
+          ))
+        )}
       </div>
 
       {store.isSettingConfirm && (
