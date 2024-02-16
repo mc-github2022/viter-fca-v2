ec2fdb6542196cedbad854941168a7b4f4ad0b68
mc-github2022 <mark.bumagat@frontlinebusiness.com.ph>
2024-02-16 08:39:42 +0800

    latest

--- src/routes/RoutesAdmin.jsx
+++ src/routes/RoutesAdmin.jsx
@@ -2,12 +2,12 @@ import { devNavUrl } from "@/components/helpers/functions-general.jsx";
 import ProtectedRouteOther from "@/components/pages/access/other/ProtectedRouteOther.jsx";
 import Profile from "@/components/pages/admin/account/Profile.jsx";
 import Discount from "@/components/pages/admin/settings/discount/Discount";
-import Students from "@/components/pages/admin/students/Students";
 import StudentViewInfo from "@/components/pages/admin/students/student-info/StudentViewInfo.jsx";
 import Clients from "@/components/pages/developer/clients/Clients.jsx";
 import StudentView from "@/components/pages/developer/clients/StudentView";
 import ClientViewInfo from "@/components/pages/developer/clients/client-info/ClientViewInfo";
 import ClientStudentViewInfo from "@/components/pages/developer/clients/student-info/ClientStudentViewInfo.jsx";
+import Students from "@/components/pages/developer/students/Students";
 
 export const routesAdmin = [
   {
