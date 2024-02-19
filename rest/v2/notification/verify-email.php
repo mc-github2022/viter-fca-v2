89aef2bd21e2876c87e9757cef0f98045ad8890f
Cyrhina <cyrenemlumabas@gmail.com>
2024-02-19 13:07:44 +0800

    udpate ni cy

--- rest/v2/notification/verify-email.php
+++ rest/v2/notification/verify-email.php
@@ -13,31 +13,48 @@ include_once("template/verify-email.php");
 
 function sendEmailVerify($link, $name, $email_old, $email, $key)
 {
-	$mail = new PHPMailer(true);
-	$mail->isSMTP();
-	$mail->Host = 'mail.frontlinebusiness.com.ph'; // SiteGround
-	$mail->Port = 465;
-	$mail->SMTPSecure = "ssl";
-	$mail->SMTPAuth = true;
-	$mail->Username =  USERNAME; // if gmail use your gmail email
-	$mail->Password = PASSWORD; // if gmail use your email password
-	$mail->Subject = VERIFY_EMAIL;
-	$mail->setFrom(USERNAME, FROM);
-	$mail->isHTML(true);
-	$mail->Body = getHtmlVerifyEmail(
-		$link,
-		$name,
-		$email_old,
-		$email,
-		$key,
-		ROOT_DOMAIN,
-		IMAGES_URL
-	);
-	$mail->addAddress($email_old);
+	//trigger exception in a "try" block
+	try {
+		$mail = new PHPMailer(true);
+		$mail->isSMTP();
+		$mail->Host = 'mail.frontlinebusiness.com.ph'; // SiteGround
+		$mail->Port = 465;
+		$mail->SMTPSecure = "ssl";
+		$mail->SMTPAuth = true;
+		$mail->Username =  USERNAME; // if gmail use your gmail email
+		$mail->Password = PASSWORD; // if gmail use your email password
+		$mail->Subject = VERIFY_EMAIL;
+		$mail->setFrom(USERNAME, FROM);
+		$mail->isHTML(true);
+		$mail->Body = getHtmlVerifyEmail(
+			$link,
+			$name,
+			$email_old,
+			$email,
+			$key,
+			ROOT_DOMAIN,
+			IMAGES_URL
+		);
 
-	if ($mail->Send()) {
-		return "Success sending email.";
-	} else {
-		return "Failed sending email.";
+		$mail->addAddress($email_old);
+
+		if ($mail->Send()) {
+			return array(
+				"mail_success" => true
+			);
+		} else {
+			return array(
+				"send_error" => "Could not send email. Please contact support.",
+				"mail_success" => false
+			);
+		}
+	}
+	//catch exception
+	catch (Exception $e) {
+		return array(
+			"mail_error" => $e->getMessage(),
+			"error_message" => "Could not authenticate. Please make sure your email and password are correct.",
+			"mail_success" => false
+		);
 	}
 }
