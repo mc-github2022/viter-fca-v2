291ecb11d53822d7e77c630e7aef110bc50c5f60
mc-github2022 <mark.bumagat@frontlinebusiness.com.ph>
2024-02-20 13:21:57 +0800

    updateed

--- src/components/pages/admin/account/Profile.jsx
+++ src/components/pages/admin/account/Profile.jsx
@@ -1,7 +1,6 @@
 import { InputText } from "@/components/helpers/FormInputs";
 import Footer from "@/components/partials/Footer.jsx";
 import Header from "@/components/partials/Header.jsx";
-import Navigation from "@/components/partials/Navigation-old.jsx";
 import ModalError from "@/components/partials/modals/ModalError";
 import ModalSuccess from "@/components/partials/modals/ModalSuccess";
 import { setError, setMessage } from "@/components/store/StoreAction";
@@ -12,6 +11,7 @@ import { FaBars, FaEye, FaEyeSlash } from "react-icons/fa";
 import { TfiLock } from "react-icons/tfi";
 import * as Yup from "yup";
 import ModalConfirmPasswordChange from "./ModalConfirmPasswordChange";
+import Navigation from "../Navigation";
 
 const Profile = () => {
   const { store, dispatch } = React.useContext(StoreContext);
@@ -108,32 +108,33 @@ const Profile = () => {
             </div>
           </div>
 
-          <div className="mt-5 bg-primary rounded-md max-w-[600px] border-line border shadow-sm relative pb-4">
-            <header className=" py-2 pr-4 flex justify-end">
+          <div className="mt-5 bg-primary rounded-md max-w-[430px]  relative pb-4">
+            {/* <header className=" py-2 pr-4 flex justify-end">
               <button className="text-lg md:hidden" onClick={handleShowSubMenu}>
                 <FaBars />
               </button>
-            </header>
+            </header> */}
+
+            <div className="mb-8 pb-5">
+              <h6 className="mb-5 text-sm">Profile</h6>
+              <p className="flex">
+                <span className="font-bold mr-2 block">First Name:</span>
+                {store.credentials.data.user_other_fname}
+              </p>
+              <p className="flex">
+                <span className="font-bold mr-2">Last Name:</span>
+                {store.credentials.data.user_other_lname}
+              </p>
+              <p className="flex">
+                <span className="font-bold mr-2">Email:</span>
+                {store.credentials.data.user_other_email}
+              </p>
+            </div>
 
             <div className=" pl-0  md:flex md:gap-5">
-              <aside
-                className={`mb-5 max-w-[150px] w-full subnav absolute md:static top-8 right-4 bg-primary z-40 shadow-sm border border-line md:shadow-none md:border-none ${
-                  show ? "block" : "hidden"
-                } md:block `}
-              >
-                <ul>
-                  <li className="active !p-0 text-center">
-                    <button className="text-sm w-full p-2">
-                      <TfiLock className="md:text-lg" />
-                      Password
-                    </button>
-                  </li>
-                </ul>
-              </aside>
-
-              <div className="px-4 w-full">
+              <div className="w-full">
                 <div className="profile__block min-h-[300px] w-full ">
-                  <h6 className="mb-5">Update password</h6>
+                  <h6 className="mb-5 text-sm">Change Password</h6>
 
                   <Formik
                     initialValues={initVal}
@@ -151,12 +152,12 @@ const Profile = () => {
                               type={showCurrentPassword ? "text" : "password"}
                               name="current_password"
                               className="account_password"
-                              placeholder="Current password"
+                              label="Current password"
                             />
                             {props.values.current_password && (
                               <button
                                 type="button"
-                                className="absolute top-1/2 -translate-y-1/2 text-base text-gray-400 right-3"
+                                className="absolute top-7 text-base text-gray-400 right-3"
                                 onClick={handleShowCurrentPassword}
                               >
                                 {showCurrentPassword ? (
@@ -173,13 +174,13 @@ const Profile = () => {
                               type={showNewPassword ? "text" : "password"}
                               name="new_password"
                               className="account_password"
-                              placeholder="New password"
+                              label="New password"
                             />
 
                             {props.values.new_password && (
                               <button
                                 type="button"
-                                className="absolute top-1/2 -translate-y-1/2 text-base text-gray-400 right-3"
+                                className="absolute top-7 text-base text-gray-400 right-3"
                                 onClick={handleShowNewPassword}
                               >
                                 {showNewPassword ? <FaEyeSlash /> : <FaEye />}
@@ -192,12 +193,12 @@ const Profile = () => {
                               type={showConfirmPassword ? "text" : "password"}
                               name="confirm_password"
                               className="account_password"
-                              placeholder="Confirm New password"
+                              label="Confirm New password"
                             />
                             {props.values.confirm_password && (
                               <button
                                 type="button"
-                                className="absolute top-1/2 -translate-y-1/2 text-base text-gray-400 right-3"
+                                className="absolute top-7 text-base text-gray-400 right-3"
                                 onClick={handleShowConfirmPassword}
                               >
                                 {showConfirmPassword ? (
