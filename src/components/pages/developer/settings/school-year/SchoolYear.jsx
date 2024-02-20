243e52de1f5a75a45c77684791e2cc42d44a47e5
Mac <macmerin24@gmail.com>
2024-02-20 13:55:00 +0800

    pulling updates

---
+++ src/components/pages/developer/settings/school-year/SchoolYear.jsx
@@ -0,0 +1,52 @@
+import ModalError from "@/components/partials/modals/ModalError";
+import ModalSuccess from "@/components/partials/modals/ModalSuccess";
+import { setIsSettingAdd } from "@/components/store/StoreAction";
+import { StoreContext } from "@/components/store/StoreContext";
+import React from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import StaffFormAddEdit from "./StaffFormAddEdit";
+import StaffList from "./StaffList";
+
+const SchoolYear = ({ index }) => {
+  const { store, dispatch } = React.useContext(StoreContext);
+  const [itemEdit, setItemEdit] = React.useState(null);
+
+  const handleAdd = () => {
+    dispatch(setIsSettingAdd(true));
+    setItemEdit(null);
+  };
+
+  if (index === 16) {
+    return (
+      <>
+        <div className="">
+          <div className="bg-primary">
+            <h2 className="mb-3">School Year</h2>
+            <p className="text-xs mb-5">
+              Set list of staff that will be available to the current school
+              year
+            </p>
+          </div>
+
+          {!store.isSettingAdd && (
+            <button
+              className="flex gap-1 items-center mt-2 text-xs hover:underline mb-5"
+              onClick={handleAdd}
+            >
+              <AiOutlinePlus /> Add New
+            </button>
+          )}
+
+          {!store.isSettingAdd && <StaffList setItemEdit={setItemEdit} />}
+
+          {store.isSettingAdd && <StaffFormAddEdit itemEdit={itemEdit} />}
+
+          {store.success && <ModalSuccess />}
+          {store.error && <ModalError />}
+        </div>
+      </>
+    );
+  }
+};
+
+export default SchoolYear;
---
+++ src/components/pages/developer/settings/school-year/StaffList.jsx
@@ -0,0 +1,173 @@
+import useQueryData from "@/components/custom-hooks/useQueryData";
+import TableLoading from "@/components/partials/TableLoading";
+import TableSpinner from "@/components/partials/spinners/TableSpinner";
+import {
+  setIsSettingAdd,
+  setSettingIsConfirm,
+  setSettingIsDelete,
+} from "@/components/store/StoreAction";
+import { StoreContext } from "@/components/store/StoreContext";
+import { BsArchive } from "react-icons/bs";
+
+import NoData from "@/components/partials/NoData.jsx";
+import ModalConfirm from "@/components/partials/modals/ModalConfirm.jsx";
+import ModalDelete from "@/components/partials/modals/ModalDelete.jsx";
+import ModalInvalidRequestError from "@/components/partials/modals/ModalInvalidRequestError";
+import React from "react";
+import { FiEdit2, FiTrash } from "react-icons/fi";
+import { MdOutlineRestore } from "react-icons/md";
+
+const StaffList = ({ setItemEdit }) => {
+  const { store, dispatch } = React.useContext(StoreContext);
+  const [dataItem, setData] = React.useState(null);
+  const [id, setId] = React.useState(null);
+  const [isArchive, setIsArchive] = React.useState(1);
+
+  const {
+    isLoading,
+    isFetching,
+    error,
+    data: staff,
+  } = useQueryData(
+    "/v2/dev-staff", // endpoint
+    "get", // method
+    "staff" // key
+  );
+
+  const handleEdit = (item) => {
+    dispatch(setIsSettingAdd(true));
+    setItemEdit(item);
+  };
+
+  const handleArchive = (item) => {
+    dispatch(setSettingIsConfirm(true));
+    setId(item.settings_staff_aid);
+    setData(item);
+    setIsArchive(0);
+  };
+
+  const handleRestore = (item) => {
+    dispatch(setSettingIsConfirm(true));
+    setId(item.settings_staff_aid);
+    setData(item);
+    setIsArchive(1);
+  };
+
+  const handleDelete = (item) => {
+    dispatch(setSettingIsDelete(true));
+    setId(item.settings_staff_aid);
+    setData(item);
+  };
+
+  return (
+    <>
+      <h5 className="text-sm">List</h5>
+
+      <div className="datalist max-w-[650px] w-full overflow-x-hidden overflow-y-auto max-h-[450px] lg:max-h-[580px] custom__scroll  poco:max-h-[640px] lg:poco:max-h-[400px]">
+        {isFetching && !isLoading && <TableSpinner />}
+
+        {!isLoading && !staff.success && error ? (
+          <ModalInvalidRequestError />
+        ) : isLoading ? (
+          <TableLoading count={20} cols={3} />
+        ) : staff?.count === 0 ? (
+          <NoData />
+        ) : (
+          !isLoading &&
+          staff.success &&
+          staff?.data.map((item, key) => (
+            <div
+              className={
+                "relative datalist__item text-xs  flex justify-between lg:items-center border-b border-line py-2 first:pt-5 lg:flex-row last:border-none"
+              }
+              key={key}
+            >
+              <div
+                className={`${
+                  item.settings_staff_is_active === 1
+                    ? "opacity-100"
+                    : "opacity-40"
+                } `}
+              >
+                <p className="mb-1">
+                  {item.settings_staff_fname} {item.settings_staff_lname}
+                </p>
+                <p className="mb-1">{item.settings_staff_email}</p>
+              </div>
+
+              <ul className="datalist__action flex items-center gap-1 pr-3 ">
+                {item.settings_staff_is_active === 1 ? (
+                  <>
+                    <li className=" ">
+                      <button
+                        className="tooltip"
+                        data-tooltip="Edit"
+                        onClick={() => handleEdit(item)}
+                      >
+                        <FiEdit2 />
+                      </button>
+                    </li>
+
+                    <li>
+                      <button
+                        className="tooltip"
+                        data-tooltip="Archive"
+                        onClick={() => handleArchive(item)}
+                      >
+                        <BsArchive />
+                      </button>
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li className=" ">
+                      <button
+                        className="tooltip"
+                        data-tooltip="Restore"
+                        onClick={() => handleRestore(item)}
+                      >
+                        <MdOutlineRestore className="text-base" />
+                      </button>
+                    </li>
+                    <li>
+                      <button
+                        className="tooltip"
+                        data-tooltip="Delete"
+                        onClick={() => handleDelete(item)}
+                      >
+                        <FiTrash />
+                      </button>
+                    </li>
+                  </>
+                )}
+              </ul>
+            </div>
+          ))
+        )}
+      </div>
+
+      {store.isSettingConfirm && (
+        <ModalConfirm
+          mysqlApiArchive={`/v2/dev-staff/active/${id}`}
+          msg={`Are you sure you want to ${
+            isArchive ? "restore" : "archive"
+          } this record?`}
+          item={`${dataItem.settings_staff_fname} ${dataItem.settings_staff_lname}`}
+          queryKey={"staff"}
+          isArchive={isArchive}
+        />
+      )}
+
+      {store.isSettingDelete && (
+        <ModalDelete
+          mysqlApiDelete={`/v2/dev-staff/${id}`}
+          msg={"Are you sure you want to delete this record?"}
+          item={`${dataItem.settings_staff_fname} ${dataItem.settings_staff_lname}`}
+          queryKey={"staff"}
+        />
+      )}
+    </>
+  );
+};
+
+export default StaffList;
