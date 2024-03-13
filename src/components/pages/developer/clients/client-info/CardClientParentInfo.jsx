1b040293f97f2d524e654531c35ba4a5d8e9bced
Cyrhina <cyrenemlumabas@gmail.com>
2024-03-13 15:03:13 +0800

    udpate ni cy

--- src/components/pages/developer/clients/client-info/CardClientParentInfo.jsx
+++ src/components/pages/developer/clients/client-info/CardClientParentInfo.jsx
@@ -50,15 +50,13 @@ const CardClientParentInfo = ({
 
   return (
     <>
-      <div className="flex justify-between items-center max-w-[620px] w-full mb-3 ">
-        <div>
-          <h3 className="">Guardian List</h3>
-          <p className="text-xs opacity-75">
-            List all parent and guardian information for the student
-          </p>
-        </div>
+      <div className="max-w-[620px] w-full mb-3 ">
+        <h3 className="">Guardian List</h3>
+        <p className="text-xs opacity-75">
+          List all parent and guardian information for the student
+        </p>
         <button
-          className="btn btn--sm mt-3 hover:underline hover:!bg-[unset] tooltip"
+          className="btn btn--sm mt-3 hover:underline hover:!bg-[unset] tooltip text-[14px]"
           data-tooltip="New"
           onClick={handleAddGuardianInfo}
         >
@@ -76,67 +74,64 @@ const CardClientParentInfo = ({
         ) : (
           guardianInfo.data.map((item, key) => (
             <React.Fragment key={key}>
-              <div className="card bg-primary border-b border-line rounded-sm mb-2 last:mb-0 relative">
-                <div className="flex gap-3 items-center mb-3">
-                  <ul>
-                    <li className="capitalize font-bold">
-                      {item.relationship_name}
-                    </li>
-                  </ul>
+              <div className="card bg-primary border-b border-line rounded-sm mb-2 last:mb-0 relative grid grid-cols-[1fr,5rem] items-center">
+                {/* list */}
+                <ul>
+                  <li className="flex gap-3 items-center mb-3 capitalize font-bold">
+                    {item.relationship_name}
+                  </li>
+                  <li className=" flex gap-2 text-xs mb-2">
+                    <PiUser className="text-base mr-1" />
+                    <span className="capitalize ">
+                      {item.guardian_salutation}.
+                    </span>
+                    {item.guardian_fname} {item.guardian_lname}
+                  </li>
 
-                  <div className="card__action absolute bottom-5 right-0  flex gap-2 ">
-                    <button
-                      className=" tooltip"
-                      data-tooltip="Edit"
-                      onClick={() => handleShowGuardianForm(item)}
-                    >
-                      <FiEdit2 className="text-[17px]" />
-                    </button>
+                  <li className=" flex gap-2 text-xs mb-2">
+                    <PiMapPinLight className="text-base" />
+                    {`${item.guardian_address}, ${item.guardian_city}, ${item.guardian_province}, ${item.guardian_country}`}
+                  </li>
 
-                    <button
-                      className=" tooltip"
-                      data-tooltip="Delete"
-                      onClick={() => handleDeleteGuardianCard(item)}
-                    >
-                      <FiTrash className="text-[17px]" />
-                    </button>
-                  </div>
+                  <li className="text-xs gap-2 items-center mb-2">
+                    <span className="flex gap-2 mb-2 ">
+                      <HiOutlineEnvelope className="text-base" />
+                      {item.guardian_email}
+                    </span>
+                    {/* <LuDot className="text-xl hidden md:block" />{" "} */}
+                    <span className="flex gap-2 mb-2 ">
+                      <CiMobile3 className="text-base" />
+                      {formatMobileNumber(item.guardian_mobile)}
+                    </span>
+                    {item.guardian_landline && (
+                      <>
+                        <LuDot className="text-xl hidden md:block" />{" "}
+                        <span className="flex  mb-2 md:mb-0">
+                          <PiPhoneThin className="text-base" />
+
+                          {formatLandlineNumber(item.guardian_landline)}
+                        </span>
+                      </>
+                    )}
+                  </li>
+                </ul>
+                <div className="card__action bottom-5 right-0 justify-end flex gap-2 ">
+                  <button
+                    className=" tooltip"
+                    data-tooltip="Edit"
+                    onClick={() => handleShowGuardianForm(item)}
+                  >
+                    <FiEdit2 className="text-[17px]" />
+                  </button>
+
+                  <button
+                    className=" tooltip"
+                    data-tooltip="Delete"
+                    onClick={() => handleDeleteGuardianCard(item)}
+                  >
+                    <FiTrash className="text-[17px]" />
+                  </button>
                 </div>
-
-                <p className=" flex gap-2 text-xs mb-2">
-                  <PiUser className="text-base  mr-1" />
-                  <span className="capitalize ">
-                    {item.guardian_salutation}.
-                  </span>
-                  {item.guardian_fname} {item.guardian_lname}
-                </p>
-
-                <p className=" flex gap-2 text-xs mb-2">
-                  <PiMapPinLight className="text-base" />
-                  {`${item.guardian_address}, ${item.guardian_city}, ${item.guardian_province}, ${item.guardian_country}`}
-                </p>
-
-                <p className="text-xs  md:flex gap-2 items-center mb-2">
-                  <span className="flex  mb-2 md:mb-0">
-                    <HiOutlineEnvelope className="text-base mr-1.5" />
-                    {item.guardian_email}
-                  </span>
-                  <LuDot className="text-xl hidden md:block" />{" "}
-                  <span className="flex  mb-2 md:mb-0">
-                    <CiMobile3 className="text-base mr-1.5" />
-                    {formatMobileNumber(item.guardian_mobile)}
-                  </span>
-                  {item.guardian_landline && (
-                    <>
-                      <LuDot className="text-xl hidden md:block" />{" "}
-                      <span className="flex  mb-2 md:mb-0">
-                        <PiPhoneThin className="text-base mr-1.5" />
-
-                        {formatLandlineNumber(item.guardian_landline)}
-                      </span>
-                    </>
-                  )}
-                </p>
               </div>
             </React.Fragment>
           ))
