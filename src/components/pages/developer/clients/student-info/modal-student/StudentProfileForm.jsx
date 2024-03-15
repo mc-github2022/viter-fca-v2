08384ff109fabdb127f0a850d2daa208168981d4
peegeetr <patrick.reyes@frontlinebusiness.com.ph>
2024-03-15 08:19:17 +0800

    updating version

--- src/components/pages/developer/clients/student-info/modal-student/StudentProfileForm.jsx
+++ src/components/pages/developer/clients/student-info/modal-student/StudentProfileForm.jsx
@@ -399,6 +399,7 @@ const StudentProfileForm = ({
                           // disabled
                         >
                           <option value="" hidden></option>
+                          <option value="0">Not Applicable</option>
                           {gradeLevel?.count > 0 ? (
                             gradeLevel?.data.map((item, key) => {
                               return (
