c0b7d8bd0ccc90c54b779ac306d5aa59fc2936f3
CyreneLumabas <cyrene.lumabas@frontlinebusiness.com.ph>
2024-10-10 07:42:02 +0800

    commit-to pull

--- rest/v2/db-backup/fcav2_settings_discount_additional.sql
+++ rest/v2/db-backup/fcav2_settings_discount_additional.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 11, 2024 at 03:55 AM
+-- Generation Time: Jul 15, 2024 at 02:20 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -37,6 +37,7 @@ CREATE TABLE `fcav2_settings_discount_additional` (
   `discount_additional_created` datetime NOT NULL,
   `discount_additional_is_early_bird` tinyint(1) NOT NULL,
   `discount_additional_is_stand_alone_discount` tinyint(1) NOT NULL,
+  `discount_additional_is_applyed_scheme_a` tinyint(1) NOT NULL,
   `discount_additional_updated` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -44,12 +45,12 @@ CREATE TABLE `fcav2_settings_discount_additional` (
 -- Dumping data for table `fcav2_settings_discount_additional`
 --
 
-INSERT INTO `fcav2_settings_discount_additional` (`discount_additional_aid`, `discount_additional_is_active`, `discount_additional_name`, `discount_additional_base_rate_id`, `discount_additional_percent`, `discount_additional_amount`, `discount_additional_created`, `discount_additional_is_early_bird`, `discount_additional_is_stand_alone_discount`, `discount_additional_updated`) VALUES
-(1, 1, 'Enable Full Payment Discount?', '4', '10', '', '2024-02-23 09:06:00', 0, 0, '2024-07-10 08:28:49'),
-(2, 1, 'Enable New Student Promo?', '4', '', '1500', '2024-02-23 09:06:36', 0, 0, '2024-07-10 08:29:01'),
-(3, 1, 'Enable Early Bird Promo?', '4', '', '', '2024-02-23 09:07:43', 1, 1, '2024-07-11 09:50:41'),
-(4, 1, 'Enable Loyalty Discount?', '4', '30', '', '2024-03-14 14:38:45', 0, 0, '2024-07-10 08:28:54'),
-(5, 1, 'Enable Re-Enrolling Promo?', '4', '', '1000', '2024-03-14 14:39:11', 0, 0, '2024-07-10 08:29:05');
+INSERT INTO `fcav2_settings_discount_additional` (`discount_additional_aid`, `discount_additional_is_active`, `discount_additional_name`, `discount_additional_base_rate_id`, `discount_additional_percent`, `discount_additional_amount`, `discount_additional_created`, `discount_additional_is_early_bird`, `discount_additional_is_stand_alone_discount`, `discount_additional_is_applyed_scheme_a`, `discount_additional_updated`) VALUES
+(1, 1, 'Enable Full Payment Discount?', '4', '10', '', '2024-02-23 09:06:00', 0, 0, 1, '2024-07-11 15:33:53'),
+(2, 1, 'Enable New Student Promo?', '4', '', '1500', '2024-02-23 09:06:36', 0, 0, 0, '2024-07-10 08:29:01'),
+(3, 1, 'Enable Early Bird Promo?', '4', '', '', '2024-02-23 09:07:43', 1, 0, 0, '2024-07-11 10:45:39'),
+(4, 1, 'Enable Loyalty Discount?', '4', '30', '', '2024-03-14 14:38:45', 0, 0, 0, '2024-07-10 08:28:54'),
+(5, 1, 'Enable Re-Enrolling Promo?', '4', '', '1000', '2024-03-14 14:39:11', 0, 0, 0, '2024-07-10 08:29:05');
 
 --
 -- Indexes for dumped tables
